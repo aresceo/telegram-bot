55cfebbe4ad3f00c9cb112b089be5965f8eee712
aresceo <aresy3p@gmail.com>
2024-12-12 14:58:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Ciao! Sono un bot Telegram.")
+bot_token = os.getenv("BOT_TOKEN")
+if not bot_token:
+    raise ValueError("Il token non è stato trovato. Controlla le variabili d'ambiente.")
 
-if __name__ == '__main__':
-    bot_token = os.getenv("7897882707:AAGjonYUrDa_qmBkNbdIoAzL2jApSQJN4ug")
-    app = ApplicationBuilder().token(bot_token).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+app = ApplicationBuilder().token(bot_token).build()
