2a8a30cf021cfa02d9e939d07e31bcba6f4e1ab2
aresceo <aresy3p@gmail.com>
2024-12-12 14:54:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Ciao! Sono un bot Telegram.")
 
 if __name__ == '__main__':
-    bot_token = os.getenv("BOT_TOKEN")
+    bot_token = os.getenv("7897882707:AAGjonYUrDa_qmBkNbdIoAzL2jApSQJN4ug")
     app = ApplicationBuilder().token(bot_token).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
